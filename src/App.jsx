2845ff9ee923e2eb7cfd5fd30894a85acc0b313e
SebaPrado <sebastian,pradomelesi@gmail.com>
2024-08-17 11:18:50 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Contact from "./components/contact/Contact";
-import Experience from "./components/experience/Experience";
+import Experience from "./components/3-skills/Experience";
 import Footer from "./components/footer/Footer";
 import Header from "./components/1-header/Header";
-import Intro from "./components/intro/Intro";
-import Portfolio from "./components/portfolio/Portfolio";
+import Intro from "./components/2-intro/Intro";
+import Portfolio from "./components/4-portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/topbar/Topbar";
 
